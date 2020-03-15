8448d33da8086bd7245788f5c6289fce5a426735
RubenOAlvarado <rubenosmaralvarado@gmail.com>
2020-03-14 18:47:31 -0600

    simple node app with test

---
+++ __test__/test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../index');
+
+
+describe('GET /', () => {
+    it('responde with hello world', (done) => {
+        request(app).get('/').expect('Hello World', done);
+    })
+});
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+
+//Responde with "hello world" for requests that hit our root
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
